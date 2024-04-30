2bcbfd6e53d992b26eb6b95da664c0b72b1da13c
Benjamin Greenfield <benjamingreenfield@Benjamins-iMac.home>
2024-04-30 18:42:33 +0100

    all the apt instal and pip install stuff for the x11 and control from python, and the supervisor conf files

--- docker/start.sh
+++ docker/start.sh
@@ -2,6 +2,10 @@
 # Start SSH Daemon
 /usr/sbin/sshd -D &
 
+cp /docker/configs/xvfb.conf /etc/supervisor/conf.d/
+supervisord -c /etc/supervisor/supervisord.conf
+
+
 # Start Jupyter Lab
 exec jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
 
